ca68983bdc9fd07f502b798aa2c002710f02b721
Vasef97 <116202606+Vasef97@users.noreply.github.com>
2024-11-14 19:10:06 +0200

    Delete videoclub_rest_api directory

--- videoclub_rest_api/src/main/java/com/ltp/videoclubs/exception/MovieNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ltp.videoclubs.exception;
-
-public class MovieNotFoundException extends RuntimeException {
-
-    public MovieNotFoundException(Long movieId, String videoclubName) {
-        super("The movie with id '" + movieId + "' does not excist in the videoclub '" + videoclubName + "'.");
-    }
-}
--- videoclub_rest_api/src/main/java/com/ltp/videoclubs/exception/VideoclubNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ltp.videoclubs.exception;
-
-public class VideoclubNotFoundException extends RuntimeException {
-
-    public VideoclubNotFoundException(Long id) {
-        super("The videoclub with id '" + id + "' does not excist");
-    }
-
-}
